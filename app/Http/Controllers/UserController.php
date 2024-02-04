4f81453e309f2c7b37f456875d704d59cae2d8ac
hilalex07 <alexandre.hilaire07@gmail.com>
2024-02-04 21:33:44 +0100

    Destroy User function CRUD completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -53,8 +53,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        if ($user){
+            $user->delete($user);
+        }
     }
 }
