a37be4da5546152233b1ed5bd1f8d062602b9545
hilalex07 <alexandre.hilaire07@gmail.com>
2024-02-04 20:24:57 +0100

    Destory category function CRUD completed

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -48,8 +48,10 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        if ($category){
+            $category->delete($category);
+        }
     }
 }
