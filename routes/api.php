947f949325ba01a4e429cae86fd9a35329f2cc51
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-02-07 14:08:06 +0100

    add exception for routes  protection

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\API\AddressController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\CrafterController;
@@ -34,31 +35,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 */
 
 // * Protected routes, you must be authentitcate
-Route::middleware(['auth:sanctum'])->group(function() {
+Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::apiResource('addresses',AddressController::class);
-    
+    Route::apiResource('addresses', AddressController::class);
+
     Route::apiResource('orders', OrderController::class);
-    
+
     Route::apiResource('users', UserController::class);
 
+    Route::apiResource('crafters', CrafterController::class)->except(["index", "show"]);
+
+    Route::apiResource('images', ImageController::class)->except(["index", "show"]);
+
+    Route::apiResource('products', ProductController::class)->except(["index", "show", "searchByCatergories"]);
+
+    Route::apiResource('categories', CategoryController::class)->except(["index", "show"]);
+
+    Route::apiResource('materials', MaterialController::class)->except(["index", "show"]);
+
+    Route::apiResource('pmodels', PmodelController::class)->except(["index", "show"]);;
+
+    // * Search filters routes
+
+    Route::get('/orders/search/{userID}', [OrderController::class, 'searchByUser']);
 });
 
-
-Route::apiResource('crafters', CrafterController::class);
-    
-Route::apiResource('images', ImageController::class);
-
-Route::apiResource('products',ProductController::class);
-
-Route::apiResource('categories', CategoryController::class);
-    
-Route::apiResource('materials', MaterialController::class);
-
-Route::apiResource('pmodels', PmodelController::class);
-
-// * Search filters routes
-
-Route::get('/products/search/{categoryId}', [ProductController::class, 'searchByCatergories']);
-
-Route::get('/orders/search/{userID}',[OrderController::class, 'searchByUser']);
+    // * Search filters routes
+    Route::get('/products/search/{categoryId}', [ProductController::class, 'searchByCatergories']);
