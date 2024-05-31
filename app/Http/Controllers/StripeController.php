1970eaa69531b0e9c7e05150ba89336ee633f450
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-05-31 16:47:13 +0200

    install stripe and make a controller for it

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+
+use Stripe\StripeClient;
+use Stripe\PaymentIntent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Stripe\Stripe as StripeGateway;
+use Throwable;
+
+class StripeController extends Controller
+{
+    public function initiatePayment(Request $request)
+    {
+        StripeGateway::setApiKey(env('STRIPE_PV_KEY'));
+
+        try {
+            $paymentIntent = PaymentIntent::create([
+                'amount' => $request->amount * 100, // Multiply as & when required
+                'currency' => $request->currency,
+                'automatic_payment_methods' => [
+                    'enabled' => true,
+                ],
+            ]);
+
+            // Save the $paymentIntent->id to identify this payment later
+        } catch (Throwable $e) {
+            report($e);
+            return false;
+        }
+
+        return [
+            'token' => (string) Str::uuid(),
+            'client_secret' => $paymentIntent->client_secret
+        ];
+    }
+}
