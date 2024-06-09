6267b3e8b31ee4ede1724228a733c8e5defda204
hilalex07 <hilalex07@hotmail.fr>
2024-06-09 11:33:07 +0200

    create materials test and test get all materials passed

---
+++ tests/Feature/MaterialTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+class MaterialTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $admin;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->artisan('migrate');
+        $this->admin = User::factory()->create(['role' => 0]);
+        $this->actingAs($this->admin);
+    }
+
+    public function testGetAllMaterials(): void {
+
+        $response = $this->get('/materials');
+
+        $response->assertStatus(200);
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+    }
+}
