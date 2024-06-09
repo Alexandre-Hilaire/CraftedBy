e287a9b1b3cc40938440a78392140c6e2f053df8
hilalex07 <hilalex07@hotmail.fr>
2024-06-09 11:44:30 +0200

    create p_models test and get all test passed

---
+++ tests/Feature/P_modelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+class P_modelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $admin;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->artisan('migrate');
+        $this->admin = User::factory()->create(['role' => 0]);
+        $this->actingAs($this->admin);
+    }
+
+    public function testgetAllP_models(): void {
+
+        $response = $this->get('/pmodels');
+
+        $response->assertStatus(200);
+    
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+    }
+}
