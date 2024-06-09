cd83c1103b5bb52682c91826344d98aee18292d7
hilalex07 <hilalex07@hotmail.fr>
2024-06-09 12:12:31 +0200

    create images test and get all test passed

---
+++ tests/Feature/Imagestest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+class Imagestest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $admin;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->artisan('migrate');
+        $this->admin = User::factory()->create(['role' => 0]);
+        $this->actingAs($this->admin);
+    }
+
+    public function getAllImages(): void {
+
+        $response = $this->get('/images');
+
+        $response->assertStatus(200);
+    }
+
+protected function tearDown(): void {
+        parent::tearDown();
+    }    
+}
