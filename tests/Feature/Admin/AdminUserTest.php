7a26193bd7b9444589b443ea28821dcd8f06a514
hilalex07 <hilalex07@hotmail.fr>
2024-06-20 16:49:13 +0200

    GetUsersCrafters test passed

--- tests/Feature/Admin/AdminUserTest.php
+++ tests/Feature/Admin/AdminUserTest.php
@@ -81,6 +81,12 @@ class AdminUserTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_getUserCrafters(): void {
+        $response = $this->get('/users/crafters/'. $this->admin->id);
+
+        $response->assertStatus(200);
+    }
+
     protected function tearDown(): void
     {
         parent::tearDown();
