9f3f84967891a564c38da3cbd9b31bee9b66ea02
hilalex07 <hilalex07@hotmail.fr>
2024-06-20 16:58:13 +0200

    Edit comments in api routing

--- routes/api.php
+++ routes/api.php
@@ -33,19 +33,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 /* 
- * apiResssource créer automatiquement les routes :
- * GET / POST : api/products
- * GET / PUT / PATCH / DELETE api/products/{product}
+ * apiResssource create automaticly routes :
+ * GET / POST / PUT|PATCH / DELETE
 */
 
 // * Protected routes, you must be authentitcate
 Route::middleware(['auth:sanctum'])->group(function () {
 
+    // * Adresses
     Route::apiResource('addresses', AddressController::class);
 
     Route::get('/addresses/search/{userId}', [AddressController::class, 'getUserAddresses']);
 
+    // * orders
+
     Route::apiResource('orders', OrderController::class);
+    
+    Route::get('/orders/search/{userID}', [OrderController::class, 'searchByUser']);
+
+    // * users
 
     Route::apiResource('users', UserController::class);
 
@@ -53,21 +59,30 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get('/users/crafters/{user}', [UserController::class, 'getUserCrafters']);
 
+    // * crafters
+
     Route::apiResource('crafters', CrafterController::class);
+
+    // * images
 
     Route::apiResource('images', ImageController::class);
 
+    // * products
+
     Route::apiResource('products', ProductController::class);
+
+     // * Categories
 
     Route::apiResource('categories', CategoryController::class);
 
+    // * materials
+
     Route::apiResource('materials', MaterialController::class);
+
+    // * pModels
 
     Route::apiResource('pmodels', PmodelController::class);
 
-    // * Search filters routes
-
-    Route::get('/orders/search/{userID}', [OrderController::class, 'searchByUser']);
 
     // * Mindee API
     Route::post('/parse-file', [MindeeController::class, 'parseFile']);
@@ -81,19 +96,25 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('invoice', [InvoicesController::class, 'createInvoice']);
 });
 
+// * Unprotected routes
 
+    // * products 
+Route::apiResource('products', ProductController::class)->only(["index", "show", "searchByCatergories"]);
 Route::get('/products/search/{categoryId}', [ProductController::class, 'searchByCatergories']);
 
+    // * crafters
 Route::apiResource('crafters', CrafterController::class)->only(["index", "show"]);
 
+    // * images
 Route::apiResource('images', ImageController::class)->only(["index", "show"]);
 
-Route::apiResource('products', ProductController::class)->only(["index", "show", "searchByCatergories"]);
-
+    // * categories
 Route::apiResource('categories', CategoryController::class)->only(["index", "show"]);
 
+    // * materials
 Route::apiResource('materials', MaterialController::class)->only(["index", "show"]);
 
+    // * pmodels
 Route::apiResource('pmodels', PmodelController::class)->only(["index", "show"]);
 
 // * AdressesAPI
